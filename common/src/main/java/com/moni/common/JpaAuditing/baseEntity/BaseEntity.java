fa44afaba6e9cf49f3d6ae4a1158c1d23c85324a
kimyounguk1 <kyw020108@gmail.com>
2026-06-14 16:11:34 +0900

    feat:#4 common모듈 구현 및 아크유닛 형식 êµ 구현

---
+++ common/src/main/java/com/moni/common/JpaAuditing/baseEntity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.moni.common.JpaAuditing.baseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    private LocalDateTime deletedAt;
+    private String deletedBy;
+
+    public void delete(String deletedBy) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = deletedBy;
+    }
+}
---
+++ common/src/main/java/com/moni/common/error/exception/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.moni.common.error.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String getCode();
+    String getMessage();
+    HttpStatus getStatus();
+}
