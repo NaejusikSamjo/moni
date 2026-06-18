e3752c065dbab800da6b588544e43d2f082c3831
Jieunbakk <jieun134@knu.ac.kr>
2026-06-18 10:55:54 +0900

    feat: common 작업

--- ai-service/src/main/java/com/moni/ai/common/jpaAuditing/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.moni.ai.common.jpaAuditing;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing(auditorAwareRef = "awareUserAudit")
-public class JpaAuditingConfig {
-}
