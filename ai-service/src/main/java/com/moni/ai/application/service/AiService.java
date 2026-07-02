eaca0ccbe0538f2a4845ebe275d60c54c7167ca9
Jieunbakk <jieun134@knu.ac.kr>
2026-07-02 15:25:38 +0900

    feat: querydsl 추가

--- ai-service/src/main/java/com/moni/ai/application/service/AiService.java
+++ ai-service/src/main/java/com/moni/ai/application/service/AiService.java
@@ -37,13 +37,11 @@ public class AiService {
         WatchCompany company = WatchCompany.fromTicker(ticker);
 
         // 1. 유효한 캐시 조회
-        // TODO: queryDSL도입 예정
-        Optional<CompanyIssueAnalysisEntity> cached = companyIssueAnalysisRepository
-                .findTopByTickerAndExpiredAtAfterOrderByCreatedAtDesc(ticker, LocalDateTime.now());
+        Optional<CompanyIssueAnalysisEntity> cached = companyIssueAnalysisRepository.findLatestValidAnalysis(ticker);
 
         if (cached.isPresent()) {
-            log.info("[{}] 캐시된 분석 결과 반환", ticker);
-            return CompanyIssueResDto.toDto(cached.get());
+            log.info("[{}] 분석 존재", ticker);
+            throw new CustomException(AiErrorCode.ANALYSIS_ALREADY_EXISTS);
         }
 
 
@@ -95,7 +93,7 @@ public class AiService {
         WatchCompany.fromTicker(ticker);
 
         return companyIssueAnalysisRepository
-                .findTopByTickerAndExpiredAtAfterOrderByCreatedAtDesc(ticker, LocalDateTime.now())
+                .findLatestValidAnalysis(ticker)
                 .map(CompanyIssueResDto::toDto)
                 .orElseThrow(() -> new CustomException(AiErrorCode.AI_NOT_FOUND));
     }
