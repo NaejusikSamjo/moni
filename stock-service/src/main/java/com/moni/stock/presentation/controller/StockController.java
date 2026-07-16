1f4e765ec2e34e2905fb754242c8181d1b6f7868
kimyounguk1 <kyw020108@gmail.com>
2026-07-16 15:52:52 +0900

    Add stock master endpoint notes

--- stock-service/src/main/java/com/moni/stock/presentation/controller/StockController.java
+++ stock-service/src/main/java/com/moni/stock/presentation/controller/StockController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,9 +33,22 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/stocks")
 @RequiredArgsConstructor
+@Slf4j
 public class StockController {
 
     private final StockQueryUseCase stockQueryUseCase;
+    private final StockMasterService stockMasterService;
+
+    @Operation(summary = "초기 코스피, 코스닥, 테마 마스터 파일 전처리, 저장 로직", description = "관리자만 호출가능, 초기 한번만 실행하면 되는 로직임.")
+    @PostMapping("/download/stocks")
+    public Map<String, String> downloadStocks() {
+        log.info("{}", "파일 다운로드 실행은 됨");
+        stockMasterService.runOnceOnStartupKosdaq();
+        stockMasterService.runOnceOnStartupKospi();
+        stockMasterService.updateThemeMasters();
+
+        return Map.of("message", "success download stocks");
+    }
 
     @Operation(summary = "주식 조회 기능 (검색)", description = "주식 조회 기능을 제공한다, 검색어는 필수가 아니다, 페이징처리가 된다.")
     @GetMapping("/search")
